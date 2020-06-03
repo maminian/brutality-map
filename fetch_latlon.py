42fa7001d7e6b8d87f8ff84326320e28e0efbda4
maminian <manuchehr.aminian@gmail.com>
2020-06-03 04:19:25 -0600

    start of fetching lat/lon to replace highlighting of whole counties

---
+++ fetch_latlon.py
@@ -0,0 +1,7 @@
+# small script to try to get a lookup for the cities we have.
+import load_data
+
+# following https://amaral.northwestern.edu/blog/getting-long-lat-list-cities
+from opencage.geocoder import OpenCageGeocode
+
+import opencage_apikey # get your own.
